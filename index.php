a16cf93554a20bbbac3c2df728aa4f4edad5b977
jacqgeorg02 <92845695+jacqgeorg02@users.noreply.github.com>
2024-01-08 23:25:14 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jacquie's Portal Page
+    </title>
+    <link href="css/styles.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+    <h1><a href="">Jacquie's Portal Page</a></h1>
+
+    <h2>The navigation below represents our BIG Assignments.</h2>
+
+<div id="wrapper">
+<nav>
+<ul>
+<li><a href="website/daily.php">Switch</a></li>
+<li><a href="">Troubleshoot</a></li>
+<li><a href="">Calculator</a></li>
+<li><a href="">Email</a></li>
+<li><a href="">Database</a></li>
+<li><a href="">Gallery</a></li>
+</ul>
+</nav>
+
+<main>
+<h2>About Jacquie</h2>
+<img class="right" src="images/jacqkel.jpg" alt="Jacquie">
+<p>I am an IBEST support instructor here at Seattle Central, and I have been an ESL teacher for almost 40 years -- the last 32 at Seattle Central. In addition to Seattle Central, I have taught at many schools up and down the West Coast of the United States, including St. Martin's College in Lacey, WA, San Diego State University, the University of California at San Diego, Washington State University and both campuses of MiraCosta Community College in Oceanside, CA and San Elijo, CA. I started my teaching career at Washington State University when I was only 23. So, I have spent the majority of my life teaching. Thankfully, I love my job.</p>
+<p>I am an IBEST support instructor here at Seattle Central, and I have been an ESL teacher for almost 40 years -- the last 32 at Seattle Central. In addition to Seattle Central, I have taught at many schools up and down the West Coast of the United States, including St. Martin's College in Lacey, WA, San Diego State University, the University of California at San Diego, Washington State University and both campuses of MiraCosta Community College in Oceanside, CA and San Elijo, CA. I started my teaching career at Washington State University when I was only 23. So, I have spent the majority of my life teaching. Thankfully, I love my job.</p>
+<h3>Jacquie's MAMP</h3>
+<img class="left" src="images/mampjacq.jpg" alt="Jacquie's MAMP">
+<h3>Jacquie's MAMP Errors</h3>
+<img class="left" src="images/mamperror.jpg" alt="Jacquie's MAMP Error">
+</main>
+
+<aside>
+<h2>Weekly Class Exercises</h2>
+<h3>Week 2</h3>
+<ul>
+<li><a href="weeks/week2/var.php">var.php</a></li>
+<li><a href="weeks/week2/var2.php">var2.php</a></li>
+<li><a href="weeks/week2/something.php">something.php</a></li>
+<li><a href="weeks/week2/something2.php">something2.php</a></li>
+</ul>
+
+
+
+</aside>
+
+<!-- close wrapper -->
+</div>
+
+<footer>
+        <ul>
+<li>Copyright &copy;
+    2024</li>
+<li>All Rights Reserved</li>
+<li><a href="../index.php">Web Design by Jacquie</a></li>
+<li><a id="html-checker" href="#">HTML Validation</a></li>
+<li><a id="css-checker" href="#">CSS Validation</a></li>
+</ul>
+
+<script>
+        document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
+        document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
+</script>
+</footer>  
+</body>
+</html>
