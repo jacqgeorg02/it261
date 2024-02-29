e03f9248640530e71a65b13f86e7a9b29464efc7
jacqgeorg02 <92845695+jacqgeorg02@users.noreply.github.com>
2024-02-29 09:50:02 -0800

    Add files via upload

---
+++ weeks/week9/data.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+user_id INT(6) AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR (15) NOT NULL,
+last_name VARCHAR (20) NOT NULL,
+username VARCHAR (25) NOT NULL,
+email VARCHAR (25) NOT NULL,
+password VARCHAR (50) NOT NULL
+)
+*
