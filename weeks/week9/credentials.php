b6ca931f563360417f2110bb0f221d0f23f53238
jacqgeorg02 <92845695+jacqgeorg02@users.noreply.github.com>
2024-03-06 22:50:52 -0800

    Add files via upload

---
+++ weeks/week9/credentials.php
@@ -0,0 +1,7 @@
+<?php
+// my credentials that will allow me to connect to the database!
+
+define('DB_NAME', 'jacqgeorg_winter2024');
+define('DB_USER', 'jwinter2024_user');
+define('DB_PASSWORD', '575Tacos!');
+define('DB_HOST', 'mysql.jacqgeorg.com');
--- weeks/week9/errors.php
+++ weeks/week9/errors.php
@@ -3,7 +3,9 @@
 // we used an array_push function because we have different messages for different errors
 // the logic is -- if we have errors, we need to display them
 
-if(count($errors) > 0): ?>
+include('config.php');
+
+if(count($errors) > 0) : ?>
 <div class="errors">
 <?php foreach($errors as $error) : ?>
     <p>
@@ -13,4 +15,4 @@ if(count($errors) > 0): ?>
 
 </div>
 <!-- end div errors -->
-<?php endif;?>
+<?php endif ;?>
