f7e2f1f0e972437e8dd581d1745098371de4b101
jacqgeorg02 <92845695+jacqgeorg02@users.noreply.github.com>
2024-03-17 09:28:26 -0700

    Add files via upload

--- final/index.php
+++ final/index.php
@@ -28,13 +28,17 @@ include('./includes/header.php');?>
 <h1>Hello</h1>
 <p>Ligula ullamcorper malesuada proin libero nunc consequat interdum. Ultrices dui sapien eget mi proin sed libero. At lectus urna duis convallis convallis tellus id interdum. Tincidunt dui ut ornare lectus sit amet. Odio ut sem nulla pharetra. Pellentesque massa placerat duis ultricies lacus sed turpis. Sit amet aliquam id diam maecenas ultricies mi eget. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum. Quam elementum pulvinar etiam non quam lacus suspendisse faucibus interdum. Morbi non arcu risus quis. Hac habitasse platea dictumst quisque sagittis purus sit amet. Tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium. Risus feugiat in ante metus dictum at tempor. Eu facilisis sed odio morbi quis commodo odio aenean sed. Proin libero nunc consequat interdum. Tellus pellentesque eu tincidunt tortor. Elementum tempus egestas sed sed risus pretium. Imperdiet proin fermentum leo vel orci porta.</p>
 <p>Morbi quis commodo odio aenean sed. Magna ac placerat vestibulum lectus. Rutrum quisque non tellus orci ac. Congue eu consequat ac felis donec. Vivamus arcu felis bibendum ut tristique. Ultrices in iaculis nunc sed augue. Tincidunt tortor aliquam nulla facilisi cras. Integer feugiat scelerisque varius morbi enim nunc faucibus a pellentesque. Eu ultrices vitae auctor eu augue ut. Quisque non tellus orci ac auctor augue mauris augue neque. Euismod elementum nisi quis eleifend quam adipiscing vitae proin. Vulputate odio ut enim blandit volutpat maecenas volutpat. Elementum eu facilisis sed odio morbi quis commodo odio. Vulputate mi sit amet mauris. Tincidunt lobortis feugiat vivamus at augue eget. Tincidunt nunc pulvinar sapien et ligula ullamcorper. Vel facilisis volutpat est velit egestas dui. In eu mi bibendum neque egestas congue. Neque aliquam vestibulum morbi blandit cursus risus at ultrices. Dui id ornare arcu odio ut sem nulla.</p>
-<p>Amet venenatis urna cursus eget nunc scelerisque viverra. Duis at tellus at urna condimentum. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum. Varius vel pharetra vel turpis nunc. Pellentesque nec nam aliquam sem et tortor consequat id. Dolor magna eget est lorem ipsum dolor sit amet. Augue neque gravida in fermentum et sollicitudin ac orci phasellus. Interdum consectetur libero id faucibus nisl tincidunt. Turpis massa sed elementum tempus. Vel eros donec ac odio tempor orci dapibus ultrices in. Enim lobortis scelerisque fermentum dui faucibus in. Integer vitae justo eget magna fermentum iaculis eu. Quam pellentesque nec nam aliquam sem et. Non curabitur gravida arcu ac tortor. Hendrerit dolor magna eget est lorem. Facilisis leo vel fringilla est ullamcorper eget. Sem et tortor consequat id porta nibh. Viverra maecenas accumsan lacus vel facilisis volutpat est velit egestas.</p>
+
 </main>
-<!-- <h3>Hello</h3>
+
 <aside>
-
-
-</aside> -->
+<figure>
+<img src="./images/whatja.jpg" alt="Jacquie in thought">
+<figcaption>
+My brain is swirling, and I can't quite catch what I'm looking for, but I know that, eventually, I will. 
+</figcaption>
+</figure>
+</aside>
 
 </div>
 <!-- close wrapper --> 
